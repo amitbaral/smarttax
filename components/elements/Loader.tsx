928e322e7b90fa8e6012a9defe505944c073ddd3
Amit Baral <supper_a@hotmail.com>
2025-06-23 11:22:33 +1000

    refactor: enhance TypeScript support by declaring custom element types for Loader component

--- components/elements/Loader.tsx
+++ components/elements/Loader.tsx
@@ -1,5 +1,21 @@
 import { useEffect } from "react";
 
+// Declare the custom element type for TypeScript
+declare global {
+    namespace JSX {
+        interface IntrinsicElements {
+            'l-ring-2': {
+                size?: string | number;
+                stroke?: string | number;
+                'stroke-length'?: string | number;
+                'bg-opacity'?: string | number;
+                speed?: string | number;
+                color?: string | number;
+            };
+        }
+    }
+}
+
 export default function Loader() {
     useEffect(() => {
         async function getLoader() {
@@ -19,11 +35,3 @@ export default function Loader() {
         ></l-ring-2>
     );
 }
-
-
-
-// import { ring2 } from 'ldrs'
-
-// ring2.register()
-
-// // Default values shown
